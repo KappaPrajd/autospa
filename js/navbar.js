8d584975c56cae2c568224853c9a458ecb6781df
Arkadiusz-Rejman <tylko213@gmail.com>
2021-02-16 20:44:45 +0100

    newnavbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,4 @@
-const navbar = document.querySelector("nav");
-const main = document.querySelector("main");
-const toggleButton = document.querySelector(".toggle-button");
-const navbarLinks = document.querySelector(".navbar-link");
-
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
-
-//place content under the fixed navbar
-const placeContent = () => {
-  main.style.paddingTop = `${navbar.offsetHeight}px`;
-};
-
-document.addEventListener("load", placeContent());
-window.onresize = placeContent;
