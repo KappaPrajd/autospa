da9fabf69eeff02fd6902fca2329568841e7efb6
KappaPrajd <tomaszmolski22@wp.pl>
2021-03-11 11:43:14 +0100

    added hamburger icon, minor bug fixes

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,21 @@
-window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+const header = document.querySelector("header");
+const hamburger = document.querySelector(".hamburger");
+const html = document.querySelector(".html");
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth > 1400) {
+    header.classList.toggle("sticky", window.scrollY > 0);
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 1400) {
+    header.classList.remove("sticky");
+  }
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  header.classList.toggle("active");
+  html.classList.toggle("hide");
 });
