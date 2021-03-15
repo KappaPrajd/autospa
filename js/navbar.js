5f0872071ba8d1d7b18e6d65e47703c0dbddf091
KappaPrajd <tomaszmolski22@wp.pl>
2021-03-15 18:15:43 +0100

    fixed some bugs, finished mobile navigation

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,33 @@
 const header = document.querySelector("header");
 const hamburger = document.querySelector(".hamburger");
-const html = document.querySelector(".html");
+const navItems = [...document.getElementsByClassName("nav-li")];
 
+//sticky navbar
 window.addEventListener("scroll", () => {
   if (window.innerWidth > 1400) {
     header.classList.toggle("sticky", window.scrollY > 0);
   }
 });
 
+//fixing bugs occuring during transitions between css breakpoints
 window.addEventListener("resize", () => {
   if (window.innerWidth <= 1400) {
     header.classList.remove("sticky");
+  } else {
+    header.classList.remove("active");
+    hamburger.classList.remove("active");
   }
 });
 
+//hamburger and header transformation
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   header.classList.toggle("active");
-  html.classList.toggle("hide");
 });
+
+navItems.forEach((el) =>
+  el.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    header.classList.remove("active");
+  })
+);
